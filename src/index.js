ea680ad31b9f8d3fcd16cb6969bc4a6942d214e6
volha <v.khviadchenia@gmail.com>
2020-10-10 20:56:32 +0200

    feat: add part of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let newString = str;
+
+    let brackets = bracketsConfig.flat();
+
+
+function es(newString,brackets) {
+for (let i = 0; i <newString.length; i++) {
+  for (let j = 0; j < brackets.length; j++) {
+  if (brackets[j] === newString[i] && brackets[j+1] === newString[i + 1]) {
+  newString = newString.slice(0, i) + newString.slice(i + 2);
+  };
+  };
+};
+if (newString.length === 0) {
+    return true;
+    };
+    if (str === newString) {
+    return false;
+    } else {
+     es();
+    };
+};
+
+
+  }
