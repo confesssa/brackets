d98b65ee55cc3e6e8f02b58115a53a43043d6880
volha <v.khviadchenia@gmail.com>
2020-10-14 04:54:20 +0200

    feat: add Solution

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
 
-    let newString = str;
-    let brackets = bracketsConfig.flat();
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let brackets = bracketsConfig[i].join('')
+        if (str.includes(brackets)) {
+            str = str.replace(brackets, '')
+            i = -1
+        }
+    }
 
 
-for (let i = 0; i <newString.length; i++) {
-  for (let j = 0; j < brackets.length; j++) {
-  if (brackets[j] === newString[i] && brackets[j+1] === newString[i + 1]) {
-  newString = newString.slice(0, i) + newString.slice(i + 2);
-  };
-  };
-};
-if (newString.length === 0) {
-    return true;
-    };
-if (str === newString) {
-    return false;
-} else {
-     return check(newString, bracketsConfig);
-    };
-
-
-
-  }
+    
+    if (str.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
